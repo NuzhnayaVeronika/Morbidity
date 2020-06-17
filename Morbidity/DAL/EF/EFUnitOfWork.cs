f6d357b3344510ff979f15950afa885c5a11dfaf
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 13:29:15 +0300

    Added implementation of a unit of work

---
+++ Morbidity/DAL/EF/EFUnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using DAL.UnitOfWork;
+
+namespace DAL.EF
+{
+    public class EFUnitOfWork
+        : IUnitOfWork
+    {
+        private PrognosisContext db;
+        private PrognosisRepository prognosisRepository;
+        private DiseaseRepository diseaseRepository;
+
+        public EFUnitOfWork(PrognosisContext context)
+        {
+            db = context;
+        }
+        public IPrognosisRepository Prognos
+        {
+            get
+            {
+                if (prognosisRepository == null)
+                    prognosisRepository = new PrognosisRepository(db);
+                return prognosisRepository;
+            }
+        }
+
+        public IDiseaseRepository Diseases
+        {
+            get
+            {
+                if (diseaseRepository == null)
+                    diseaseRepository = new DiseaseRepository(db);
+                return diseaseRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Morbidity/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IPrognosisRepository Prognos { get; }
+        public IDiseaseRepository Diseases { get; }
+        void Save();
+    }
+}
