9a5d46ea7e96b6c5a3edcfa7869a6ab7119e05b4
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 11:49:48 +0300

    Added Contracts of Repositories

---
+++ Morbidity/DAL/Repositories/Interfaces/IDiseaseRepository.cs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces
+{
+    internal interface IDiseaseRepository
+        : IRepository<Disease>
+    {
+    }
+}
