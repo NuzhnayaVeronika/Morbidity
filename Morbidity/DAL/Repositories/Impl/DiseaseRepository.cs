c259196431ade55db2f8a2e4b9bb55b7db49aa92
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 12:35:14 +0300

    Added implementation

---
+++ Morbidity/DAL/Repositories/Impl/DiseaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using DAL.EF;
+
+namespace DAL.Repositories.Impl
+{
+    internal class DiseaseRepository
+        : BaseRepository<Disease>, IDiseaseRepository
+    {
+        internal DiseaseRepository(PrognosisContext context) 
+            : base(context)
+        {
+        }
+    }
+}
---
+++ Morbidity/DAL/Repositories/Impl/PrognosisRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using DAL.EF;
+
+namespace DAL.Repositories.Impl
+{
+    public class PrognosisRepository
+        : BaseRepository<Prognosis>, IPrognosisRepository
+    {
+        internal PrognosisRepository(PrognosisContext context) 
+            : base(context)
+        {
+        }
+    }
+}
