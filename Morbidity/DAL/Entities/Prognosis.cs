1e0667465097bcea7953d0f01345027c57d44de7
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 11:36:11 +0300

    Added Entities to DAL

---
+++ Morbidity/DAL/Entities/Prognosis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Prognosis
+    {
+        public int PrognosisID { get; set; }
+        public DateTime Date { get; set; }
+        public int DiseaseID { get; set; }
+        public char Expected { get; set; }
+    }
+}
