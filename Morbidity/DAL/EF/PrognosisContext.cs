e9dadc8ef3c1d6e726c40ca11bb9f278a041af20
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 12:32:56 +0300

    Installed Microsoft.EntityFrameworkCore

---
+++ Morbidity/DAL/EF/PrognosisContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF
+{
+    public class PrognosisContext
+        : DbContext
+    {
+        public DbSet<Prognosis> Prognos { get; set; }
+        internal DbSet<Disease> Diseases { get; set; }
+
+        public PrognosisContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
