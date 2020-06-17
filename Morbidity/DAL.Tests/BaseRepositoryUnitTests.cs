f258708623420cb8703898cb0dd23b1df819d360
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 14:23:57 +0300

    Created the required classes and added a library Moq

---
+++ Morbidity/DAL.Tests/BaseRepositoryUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DAL.Repositories.Impl;
+using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Xunit;
+using Moq;
+
+namespace DAL.Tests
+{
+    public class BaseRepositoryUnitTests
+    {
+    }
+}
---
+++ Morbidity/DAL.Tests/TestDiseaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Tests
+{
+    class TestDiseaseRepository
+        : BaseRepository<Disease>
+        {
+        public TestDiseaseRepository(DbContext context)
+            : base(context)
+            {
+            }
+        }
+}
