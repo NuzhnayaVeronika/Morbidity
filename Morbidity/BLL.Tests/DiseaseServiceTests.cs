d39447576992736c05826a771c0e66e1034a1d1a
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 20:48:02 +0300

    Added a check to convert Disease objects to DiseaseDTO objects

--- Morbidity/BLL.Tests/DiseaseServiceTests.cs
+++ Morbidity/BLL.Tests/DiseaseServiceTests.cs
@@ -12,6 +12,7 @@ using CCL.Security;
 using CCL.Security.Identity;
 using Moq;
 using Xunit;
+using System.Linq;
 
 namespace BLL.Tests
 {
@@ -40,6 +41,47 @@ namespace BLL.Tests
             // Act
             // Assert
             Assert.Throws<MethodAccessException>(() => diseaseService.GetDiseases(0));
+        }
+
+        [Fact]
+        public void GetDiseases_DiseaseFromDAL_CorrectMappingToDiseaseDTO()
+        {
+            // Arrange
+            User user = new Researcher("test", 1);
+            SecurityContext.SetUser(user);
+            var diseaseService = GetDiseaseService();
+
+            // Act
+            var actualDiseaseDto = diseaseService.GetDiseases(0).First();
+
+            // Assert
+            Assert.True(
+                actualDiseaseDto.ID == 1
+                && actualDiseaseDto.Name == "testName"
+                );
+        }
+
+        IDiseaseService GetDiseaseService()
+        {
+            var mockContext = new Mock<IUnitOfWork>();
+            var expectedDisease = new Disease() {ID = 1, Name = "testName"};
+            var mockDbSet = new Mock<IDiseaseRepository>();
+            mockDbSet.Setup(z => 
+                z.Find(
+                    It.IsAny<Func<Disease,bool>>(), 
+                    It.IsAny<int>(), 
+                    It.IsAny<int>()))
+                  .Returns(
+                    new List<Disease>() { expectedDisease }
+                    );
+            mockContext
+                .Setup(context =>
+                    context.Diseases)
+                .Returns(mockDbSet.Object);
+
+            IDiseaseService diseaseService = new DiseaseService(mockContext.Object);
+
+            return diseaseService;
         }
     }
 }
